ddabaefbec350ff490d75e4eafdd79bef9a68c1d
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-02-01 22:45:04 +0530

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -28,3 +28,19 @@ result = tf.multiply(x1, x2)
 
 print(result)
 
+
+
+#3
+
+import tensorflow as tf
+
+x1 = tf.constant([1,2,3,4])
+x2 = tf.constant([5,6,7,8])
+
+result = tf.multiply(x1, x2)
+
+sess = tf.Session()
+
+print(sess.run(result))
+
+sess.close()
